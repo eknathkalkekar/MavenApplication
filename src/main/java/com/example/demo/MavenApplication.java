67eed53c1b62e1f33e5357f48dfdc5f881a805bc
Eknath kalkekar <kalkekare@gmail.com>
2023-10-26 17:59:00 +0530

    delete

--- src/main/java/com/example/demo/MavenApplication.java
+++ src/main/java/com/example/demo/MavenApplication.java
@@ -8,14 +8,7 @@ public class MavenApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MavenApplication.class, args);
-                int a=10;
-                int b=20
-                c=a+b;
-                System.out.println(c);
-                c=a-b;
-
-        public void add(){
-        }
+             
 
 
 
